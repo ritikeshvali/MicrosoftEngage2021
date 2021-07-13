581c3eb13948f338f49ee21dd9f89a065ee640f8
Ritikesh vali <32462019+ritikeshvali@users.noreply.github.com>
2021-07-13 23:33:10 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+ 
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.broadcast.to(roomId).emit('user-connected', userId);
+
+    //Send messages to the chatting area using your username
+    socket.on('message', (message,username) => {
+      io.to(roomId).emit('createMessage', message, username);
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(process.env.PORT||3030)
